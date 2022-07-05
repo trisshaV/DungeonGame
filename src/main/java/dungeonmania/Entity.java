2238b2e58a8f96e3716af05ffced353a5992d6e1
Rui <r.ekanayake@student.unsw.edu.au>
2022-07-05 13:13:36 +1000

    Implemented sample movement for player

--- src/main/java/dungeonmania/Entity.java
+++ src/main/java/dungeonmania/Entity.java
@@ -1,8 +1,47 @@
 package dungeonmania;
 
+import dungeonmania.util.Position;
+
 /**
  * The collection of all entities that are present in dungeon.
  */
 public class Entity {
     
+    /**
+	 * ID
+	 */
+	private String id;
+    
+	/**
+	 * position
+	 */
+	private Position position;
+
+	/**
+	 * @return the position
+	 */
+	public Position getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
 }
