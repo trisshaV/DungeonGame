19163effeb01ade5b99a60ad746747af09f2f110
Rui <r.ekanayake@student.unsw.edu.au>
2022-07-05 11:31:55 +1000

    Added sample test for player movement

---
+++ src/test/java/dungeonmania/PlayerTests.java
@@ -0,0 +1,32 @@
+package dungeonmania;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class PlayerTests {
+    @Test
+    @DisplayName("Test the player can move down")
+    public void testMovementDown() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("/dungeons/d_movementTest_testMovementDown", "/configs/c_movementTest_testMovementDown");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        // create the expected result
+        EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(1, 2), false);
+
+        // move player downward
+        DungeonResponse actualDungonRes = dmc.tick(Direction.DOWN);
+        EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+
+        // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+}
