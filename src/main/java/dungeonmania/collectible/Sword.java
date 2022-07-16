adc3fe7b87519665a6a5e261f1177f0a197be8c2
Ryan Lee <ryanleeee2013@gmail.com>
2022-07-16 22:12:04 +1000

    added new test for spider

--- src/main/java/dungeonmania/collectible/Sword.java
+++ src/main/java/dungeonmania/collectible/Sword.java
@@ -35,12 +35,6 @@ public class Sword extends Collectible {
     }
 
     @Override
-    public void use() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
     public String getType() {
         return "sword";
     }
