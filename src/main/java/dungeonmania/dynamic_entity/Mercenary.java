482123c1225c41cce05466c14dd601588181a584
Ryan Lee <ryanleeee2013@gmail.com>
2022-07-16 22:42:48 +1000

    implementing random movement

--- src/main/java/dungeonmania/dynamic_entity/Mercenary.java
+++ src/main/java/dungeonmania/dynamic_entity/Mercenary.java
@@ -40,17 +40,14 @@ public class Mercenary extends DynamicEntity {
 
     public void updatePos(Direction d, List<Entity> l) {
         if (status.equals("HOSTILE")) {
-            // Check potion status
-            /*
-            if (Potion status is invisible) {
+            Player p = (Player)l.stream().filter(x -> x instanceof Player).findFirst().orElse(null);
+            if (p.getStatus().equals("INVISIBLE")) {
                 randomHostile(l);
-            } else if (Potion status is invunerable) {
-                runHostile(l);
+            } else if (p.getStatus().equals("INVINCILBLE")) {
+                randomHostile(l);
             } else {
                 chaseHostile(l);
             }
-            */
-            chaseHostile(l);
         }
     }
     
