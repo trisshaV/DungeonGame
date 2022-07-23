6931b7ad842bcbf2e9b8720f310046e171e3dbe9
Rhonzu Gau <Rhonzu@gmail.com>
2022-07-23 10:53:23 +1000

    added sun stone

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -6,6 +6,7 @@ import dungeonmania.collectible.Collectible;
 import dungeonmania.collectible.InvincibilityPotion;
 import dungeonmania.collectible.InvisibilityPotion;
 import dungeonmania.collectible.Key;
+import dungeonmania.collectible.SunStone;
 import dungeonmania.collectible.Sword;
 import dungeonmania.collectible.Treasure;
 import dungeonmania.collectible.Wood;
@@ -226,6 +227,9 @@ public class DungeonManiaController {
                     partner.setLinkPosition(newPortal.getPosition());
                     newPortal.setLinkPosition(partner.getPosition());
                 }
+                break;
+            case "sun_stone":
+                newEntity = new SunStone(id, position, jsonConfig);
                 break;
         default:
             return;
