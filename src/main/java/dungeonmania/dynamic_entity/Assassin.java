0d4f27acc923f8b3c1e78aac4dad4b75d8b0c6a6
Trissha <trisshavarman@gmail.com>
2022-07-26 21:10:15 +1000

    implemented interact method for assassin

--- src/main/java/dungeonmania/dynamic_entity/Assassin.java
+++ src/main/java/dungeonmania/dynamic_entity/Assassin.java
@@ -1,9 +1,11 @@
 package dungeonmania.dynamic_entity;
 
 import java.util.List;
+import java.util.Random;
 
 import dungeonmania.Entity;
 import dungeonmania.SerializableJSONObject;
+import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
@@ -24,6 +26,29 @@ public class Assassin extends DynamicEntity{
         this.bribeFailRate = jsonConfig.getDouble("assassin_bribe_fail_rate");
     }
 
+
+    @Override
+    public void interact (Player player) throws InvalidActionException {
+        // Check the radius
+        Position distance = Position.calculatePositionBetween(player.getPosition(), this.getPosition());
+        double radius = Math.sqrt(Math.pow(distance.getX(), 2) + Math.pow(distance.getY(), 2));
+        if (radius > bribeRadius) {
+            throw new InvalidActionException("Too far from Assassin");
+        }
+        // check coins
+        if (player.getCoins() < bribeAmount) {
+            throw new InvalidActionException("Not enough coins");
+        }
+        // Checks probability that bribe will fail:
+        double random = new Random().nextDouble();
+        if (random < bribeFailRate) {
+            throw new InvalidActionException("Unlucky! The Assassin did not accept your bribe");
+        } else {
+            status = "FRIENDLY";
+        }
+    }
+
+
     @Override
     public void updatePos(Direction d, List<Entity> l) {
         // TODO Auto-generated method stub
@@ -32,8 +57,7 @@ public class Assassin extends DynamicEntity{
 
     @Override
     public String getType() {
-        // TODO Auto-generated method stub
-        return null;
+        return "assassin";
     }
     
 }
