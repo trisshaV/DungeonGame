d8be1f1f8f9c85cef12fcc9299076c16b4ffd814
Lawrence Chau <l.chau@student.unsw.edu.au>
2022-07-27 22:37:34 +1000

    Swamp tile

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -35,6 +35,7 @@ import dungeonmania.static_entity.Exit;
 import dungeonmania.static_entity.FloorSwitch;
 import dungeonmania.static_entity.Portal;
 import dungeonmania.static_entity.StaticEntity;
+import dungeonmania.static_entity.SwampTile;
 import dungeonmania.static_entity.Wall;
 import dungeonmania.static_entity.ZombieToastSpawner;
 import java.io.File;
@@ -242,6 +243,9 @@ public class DungeonManiaController implements Serializable {
                 case "sun_stone":
                 newEntity = new SunStone(id, position, jsonConfig);
                 break;
+            case "swamp_tile":
+                newEntity = new SwampTile(id, position, Integer.valueOf(jsonEntity.getString("movement_factor")));
+                break;
         default:
             return;
         }
@@ -385,6 +389,15 @@ public class DungeonManiaController implements Serializable {
                 y.updatePos(null, entities);
             }
         );
+
+        // check for swamp tiles
+        entities.stream().filter(it -> (it instanceof SwampTile)).forEach(
+            x -> {
+                SwampTile y = (SwampTile) x;
+                y.tick();
+            }
+        );
+
         if (this.observer.checkBattle(entities) == true) {
             entities = removeDeadEntities();
             if (entities.stream().filter(it -> it instanceof Player).findFirst().orElse(null) == null) {
@@ -447,6 +460,15 @@ public class DungeonManiaController implements Serializable {
                 y.updatePos(movementDirection, entities);
             }
         );
+        
+        // check for swamp tiles
+        entities.stream().filter(it -> (it instanceof SwampTile)).forEach(
+            x -> {
+                SwampTile y = (SwampTile) x;
+                y.tick();
+            }
+        );
+        
         if (this.observer.checkBattle(entities)) {
             entities = removeDeadEntities();
             if (entities.stream().filter(it -> it instanceof Player).findFirst().orElse(null) == null) {
